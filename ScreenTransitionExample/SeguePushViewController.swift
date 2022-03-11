7a3227eb00a13040077ba6914e74485808b4dfab
Gahyun <applemeansyou@gmail.com>
2022-03-11 21:34:35 +0900

    버튼 클릭시 화면 이동

---
+++ ScreenTransitionExample/SeguePushViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SeguePushViewController.swift
+//  ScreenTransitionExample
+//
+//  Created by Gahyun Song on 2022/03/11.
+//
+
+import UIKit
+
+class SeguePushViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
